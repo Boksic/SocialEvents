c7224e481a138e7d5f13bd4639b678ba09a46349
Anthony LAMMENS <anthony.lammens@ynov.com>
2016-12-15 09:36:12 +0100

    Change - ajout des informations pour la connexion à facebook

--- app/src/main/java/com/nlrd/socialevents/MainActivity.java
+++ app/src/main/java/com/nlrd/socialevents/MainActivity.java
@@ -2,13 +2,38 @@ package com.nlrd.socialevents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.GraphRequestAsyncTask;
+import com.facebook.AccessToken;
+import com.facebook.HttpMethod;
 
 public class MainActivity extends AppCompatActivity
 {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize the SDK before executing any other operations,
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        /*GraphRequestAsyncTask graphRequest = new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/search?&type=event&q=Afterwork",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        int toto = 5;
+                    }
+                }
+        ).executeAsync();*/
     }
+
 }
