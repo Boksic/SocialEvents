fcd18ac35dae35ff610740c8d7ebb3227116dbb6
Anthony LAMMENS <anthony.lammens@ynov.com>
2017-01-25 11:38:56 +0100

    Change - ajout bouton connexion facebook

--- app/src/main/java/com/nlrd/socialevents/MainActivity.java
+++ app/src/main/java/com/nlrd/socialevents/MainActivity.java
@@ -1,14 +1,10 @@
 package com.nlrd.socialevents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.GraphRequestAsyncTask;
-import com.facebook.AccessToken;
-import com.facebook.HttpMethod;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity
 {
@@ -19,21 +15,15 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Initialize the SDK before executing any other operations,
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
 
-        /*GraphRequestAsyncTask graphRequest = new GraphRequest(
-                AccessToken.getCurrentAccessToken(),
-                "/search?&type=event&q=Afterwork",
-                null,
-                HttpMethod.GET,
-                new GraphRequest.Callback() {
-                    public void onCompleted(GraphResponse response) {
-                        int toto = 5;
-                    }
-                }
-        ).executeAsync();*/
+
     }
+
+    public void goToConfiguration(View view) {
+        Intent intent = new Intent(this, ConfigurationActivity.class);
+        startActivity(intent);
+    }
+
+
 
 }
